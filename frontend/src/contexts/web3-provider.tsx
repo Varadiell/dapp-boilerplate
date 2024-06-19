822a5a20de5372ae02c7923a596d786159e0c873
William SIMON--VEZO <william.simonvezo@gmail.com>
2024-06-19 17:49:09 +0200

    custom styles for connect wallet button

--- frontend/src/contexts/web3-provider.tsx
+++ frontend/src/contexts/web3-provider.tsx
@@ -13,8 +13,28 @@ export function Web3Provider({ children } : { children: React.ReactNode }) {
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <ConnectKitProvider
+          customTheme={{
+            "--ck-connectbutton-border-radius": "6px",
+            "--ck-connectbutton-background": "hsl(var(--muted))",
+            "--ck-connectbutton-color": "hsl(var(--accent-foreground))",
+            "--ck-connectbutton-hover-background": "hsl(var(--accent))",
+            "--ck-connectbutton-active-background": "hsl(var(--background))",
+            "--ck-connectbutton-balance-background": "hsl(var(--background))",
+            "--ck-connectbutton-balance-color": "hsl(var(--accent-foreground))",
+            "--ck-connectbutton-balance-hover-background": "hsl(var(--muted))",
+            "--ck-connectbutton-balance-active-background": "hsl(var(--background))",
+            "--ck-connectbutton-box-shadow": "0 0 0 1px hsl(var(--input))",
+            "--ck-connectbutton-balance-box-shadow": "0 0 0 1px hsl(var(--input))",
+            "--ck-connectbutton-hover-box-shadow": "0 0 0 1px hsl(var(--input))",
+            "--ck-connectbutton-balance-hover-box-shadow": "0 0 0 1px hsl(var(--input))",
+            "--ck-connectbutton-active-box-shadow": "0 0 0 1px hsl(var(--input))",
+            "--ck-connectbutton-balance-active-box-shadow": "0 0 0 1px hsl(var(--input))",
+          }}
+          options={{
+            language: 'en-US',
+          }}
           mode="auto"
-          theme="midnight"
+          theme="auto"
         >
           {children}
         </ConnectKitProvider>
