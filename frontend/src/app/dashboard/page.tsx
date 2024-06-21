08e1e25b2aa8c8526226fe62390a92a4526fc2c0
William SIMON--VEZO <william.simonvezo@gmail.com>
2024-06-22 01:01:57 +0200

    winning-proposal component

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,39 +1,18 @@
-'use client';
-
 import { AccountInfo } from '@/components/shared/account-info';
 import { ChairPerson } from '@/components/shared/chair-person';
 import { DelegateCard } from '@/components/shared/delegate-card';
 import { GiveRightToVoteCard } from '@/components/shared/give-right-to-vote-card';
 import { ProposalsList } from '@/components/shared/proposals-list';
 import { VoteCard } from '@/components/shared/vote-card';
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { DataContext } from '@/contexts/data-provider';
-import { useContext } from 'react';
+import { WinningProposal } from '@/components/shared/winning-proposal';
 
 export default function Dashboard() {
-  const {
-    data: { chairPerson, winnerName, winningProposal },
-  } = useContext(DataContext);
-
   return (
     <>
       <h1>Dashboard</h1>
       <AccountInfo />
       <ChairPerson />
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-2xl font-semibold leading-none tracking-tight">
-            Winning proposal{' '}
-            {winningProposal !== undefined && <>#{winningProposal}</>}
-          </CardTitle>
-          <CardDescription>Proposal: {winnerName}</CardDescription>
-        </CardHeader>
-      </Card>
+      <WinningProposal />
       <ProposalsList />
       <GiveRightToVoteCard />
       <DelegateCard />
